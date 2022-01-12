16ff98e5d17b18c05bc89967ee3b7f9151850b70
Thiet Nguyen <nguyenthiet1225@gmail.com>
2022-01-12 11:27:22 +0700

    new pj

---
+++ Array_exercise_3.js
@@ -0,0 +1,18 @@
+let str = prompt('Enter your word');
+let UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let LOWER = 'abcdefghijklmnopqrstuvwxyz';
+let result = [];
+
+for (let x=0;x<str.length;x++){
+    if(UPPER.indexOf(str[x]) !==-1){
+        result.push(str[x].toLowerCase());
+    }
+    else if(LOWER.indexOf(str[x]) !==-1){
+        result.push(str[x].toUpperCase());
+    }
+    else{
+        result.push(str[x]);
+    }
+}
+console.log(result.join(''));
+document.write(result.join(''))
